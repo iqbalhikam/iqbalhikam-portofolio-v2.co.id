4363e8603cf26f0c7952fc9f10c16ad8cba7f596
iqbal hikam <idiscrus@gmail.com>
2025-10-19 17:35:07 +0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://iqbalhikam-portofolio-v2.vercel.app', // GANTI DENGAN DOMAIN ANDA
+    generateRobotsTxt: true, // (Opsional) Membuat robots.txt juga
+    // ...opsi lainnya
+}
