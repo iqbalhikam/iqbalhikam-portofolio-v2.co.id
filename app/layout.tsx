8ea5105e0a59fbf1001fd7a0201a9f3c148deacd
M Iqbal Fatkhul Hikam <147716322+iqbalhikam@users.noreply.github.com>
2025-11-07 09:47:46 +0700

    [ADD]: Verification Google Search

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      
+      <meta name="google-site-verification" content="PZL4uGT9Q-1b9Z_esdgt72G0Z-I55Dl1bDofkuuYMSU" />
       <body className={inter.className}>
         {children}
         <SpeedInsights />
