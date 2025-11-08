6db92e9403619bf9f082aa172f7e90e050a6d1bd
iqbal hikam <idiscrus@gmail.com>
2025-11-09 01:27:50 +0700

    replace aboutMe

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client';
 
 import { useShowSection } from '@/hooks/useShowSection';
@@ -14,10 +15,9 @@ const About = () => {
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="flex flex-col items-center justify-center">
             <h2 className="text-4xl font-bold py-10">About Me</h2>
             <p className="text-center px-50">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tempora tempore numquam quo amet distinctio id voluptates. Magnam vero ratione ducimus commodi tempora impedit veniam ex assumenda velit ut. Quis voluptatem quos
-              expedita dignissimos officia vel accusamus explicabo debitis quaerat, id, ab voluptas culpa suscipit. Quos, a! Repudiandae quis illo recusandae iusto nemo vitae eum aliquam, veniam quas maiores suscipit! Quos vitae, quae
-              dignissimos velit eum similique iure quas ipsum nam sit explicabo voluptatum amet ratione tempore laudantium dolor, pariatur, impedit accusamus qui? Et laudantium reiciendis ea cumque ab voluptatum. Corporis sed unde
-              asperiores eum facilis voluptatem iste similique libero?
+              I'm M Iqbal Fatkhul Hikam, a passionate and creative student. I have a deep interest in programming, which motivates me to continuously learn and develop. I graduated from Bina Insani University. I believe that
+              self-development is the key to achieving goals, and I'm always looking for opportunities to hone my skills and knowledge. I believe that every day is an opportunity to learn something new and create meaningful moments. In my
+              life, I strive to be a diligent person. I hope to contribute positively to the world around me and continue to grow as an individual.
             </p>
           </motion.div>
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="flex flex-col items-center justify-center">
