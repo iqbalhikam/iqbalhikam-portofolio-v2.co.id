48461bf5f92515c57ed3f421c745a18220897a02
iqbal hikam <idiscrus@gmail.com>
2025-11-09 01:24:13 +0700

    fix bug profile image

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -36,7 +36,7 @@ const AboutMe = () => {
                   className="absolute inset-0 w-full h-full object-cover 
                group-hover:scale-110 
                transition-all duration-500 ease-in-out z-10"
-                  src="https://avatars.githubusercontent.com/u/147716322?v=4"
+                  src="/assets/iqbal1.jpg"
                   alt="Foto Profil M Iqbal"
                   fill
                   style={{ objectFit: 'cover' }}
